556edaab6dc644af333b249a115c165e79fc0aeb
Pratham Siddhpura <prathamsidd19@gmail.com>
2024-05-31 11:44:20 +0530

    did some changes to auth file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,57 +2,82 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../shared/auth.service';
 import { CommonModule } from '@angular/common';
-import { Subscription } from 'rxjs';
-import { ChangeDetectorRef } from '@angular/core';
 import { User } from '../shared/user.model';
 import { FormsModule } from '@angular/forms';
 import { NgFor } from '@angular/common';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  constructor(private activeRoute:ActivatedRoute,private router:Router,private authservice:AuthService,private cd: ChangeDetectorRef){ }
-  
-  isAuthenticated:boolean=false;
-  user:User;
-  test_var:boolean=false;
-  goToAuth()
-  {
-    this.router.navigate(['/auth'],{relativeTo:this.activeRoute})
-  }
-  ngOnInit()
-  {
-    this.authservice.isAuthenticated.subscribe({
-      next:(res)=>{
-        this.isAuthenticated=res;
-      }
-    })
-    this.authservice.user.subscribe({
-      next:(res)=>{
-        this.user=res;
-      }
-    })
-    console.log(this.user);
-    console.log(this.isAuthenticated);
-    // console.log(this.user._tokenExpirationDate)
-    console.log(new Date())
-  }
-  addcoinid(form:NgForm)
-  {
-    if(this.user && this.isAuthenticated==true && new Date() < this.user._tokenExpirationDate)
-    {
-      console.log(this.user);
-      console.log(this.isAuthenticated);
-      console.log(this.user._tokenExpirationDate);
-      
-    }
-    console.log(form.form.value);
-  
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private router: Router,
+    private authservice: AuthService,
+    private http: HttpClient
+  ) {}
+
+  isAuthenticated: boolean = false;
+  user: User;
+  test_var: boolean = false;
+  canAdd: boolean = false;
+  coinid_array: string[]=[];
+
+
+
+  goToAuth() {
+    this.router.navigate(['/auth'], { relativeTo: this.activeRoute });
   }
 
+
+  ngOnInit() {
+    this.authservice.isAuthenticated.subscribe({
+      next: (res) => {
+        this.isAuthenticated = res;
+      },
+    });
+    this.authservice.user.subscribe({
+      next: (res) => {
+        this.user = res;
+      },
+    });
+    // console.log(this.user);
+    // console.log(this.isAuthenticated);
+    if (
+      this.user &&
+      this.isAuthenticated == true &&
+      new Date() < this.user._tokenExpirationDate
+    ) {
+      this.canAdd = true;
+    }
+  }
+
+
+  addcoinid(myform: NgForm) {
+    if (this.canAdd == true) {
+
+      console.log('Adding');
+      this.coinid_array.push(myform.form.value.coinid);
+
+      this.http
+        .post(`https://crypto-sakha-default-rtdb.firebaseio.com/${this.user.id}.json`,this.coinid_array)
+        .subscribe({
+          next: (res) => {console.log(res);},
+          error: (error: Error) => {console.log(error);},
+        });
+        this.http.get(`https://crypto-sakha-default-rtdb.firebaseio.com/${this.user.id}.json`).subscribe({
+          next:(res)=>{
+            console.log(res)
+          }
+        })
+    }
+  }
 }
+      // console.log(this.user);
+      // console.log(this.isAuthenticated);
+      // console.log(this.user._tokenExpirationDate);
